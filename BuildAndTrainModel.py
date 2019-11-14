c5a8f055054bd60e2e56b992c4536e577bb2c7b2
root <root@virtualserver01.UC-Berkeley.cloud>
2019-11-14 01:56:11 +0000

    py scripts created so training can be done in the background

---
+++ BuildAndTrainModel.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#####
+# Packages
+##### 
+
+import numpy as np
+import pandas as pd 
+import pickle
+from time import time
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer 
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+cleanfilename = 'cleandata.pkl'
+modelfilename = 'model.json'
+weightsfilename = 'model.h5'
+historyfilename = 'history.pkl'
+
+
+# In[2]:
+
+
+print('Loading training data.')
+
+
+# In[3]:
+
+
+clean_data = pickle.load( open( cleanfilename , "rb" ) )
+
+
+# In[4]:
+
+
+print('Training data loaded. Shape is', clean_data.shape)
+
+
+# In[27]:
+
+
+print('Starting to build the embedding index.')
+
+
+# In[28]:
+
+
+embeddings_index = {}
+f = open('data/glove/glove.6B/glove.6B.300d.txt')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+
+# In[29]:
+
+
+print('Built embeddings index. Found %s word vectors.' % len(embeddings_index))
+
+
+# In[30]:
+
+
+def doc2seq(texts, MAX_SEQUENCE_LENGTH, EMBEDDING_DIM):
+    tokenizer = Tokenizer(filters='"#$%&()*+-/<=>@[\\]^_`{|}~\t\n')
+    tokenizer.fit_on_texts(texts)
+    sequences = tokenizer.texts_to_sequences(texts)
+
+    word_index = tokenizer.word_index
+    index_word = tokenizer.index_word
+    print('Found %s unique tokens.' % len(word_index))
+
+    data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH, padding = 'post')
+    
+    embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+    return data, embedding_matrix, word_index, index_word
+
+
+# In[31]:
+
+
+print('Building text')
+
+
+# In[32]:
+
+
+MAX_TEXT_LENGTH = 100
+EMBEDDING_DIM = 300
+data = clean_data.text
+
+x_data, encoder_emb, x_word_index, x_index_word = doc2seq(data, MAX_TEXT_LENGTH, EMBEDDING_DIM)
+
+
+# In[33]:
+
+
+print('Text built. Lengths of x_data, encoder_emb, x_word_index, x_index_word are', 
+      list(map(lambda a:len(a), [x_data, encoder_emb, x_word_index, x_index_word ])))
+
+
+# In[34]:
+
+
+print('Building headlines ')
+
+
+# In[35]:
+
+
+MAX_HEADLINE_LENGTH = 30
+EMBEDDING_DIM = 300
+data = clean_data.headline
+
+y_data, decoder_emb, y_word_index, y_index_word = doc2seq(data, MAX_HEADLINE_LENGTH, EMBEDDING_DIM)
+
+
+# In[36]:
+
+
+print('Headlines built. Lengths of y_data, decoder_emb, y_word_index, y_index_word are', 
+      list(map(lambda a:len(a), [y_data, decoder_emb, y_word_index, y_index_word])))
+
+
+# In[37]:
+
+
+x_train, x_test_temp, y_train, y_test_temp = train_test_split(x_data, y_data, 
+                                                            test_size=0.3, random_state=0) 
+
+
+# In[38]:
+
+
+x_dev, x_test, y_dev, y_test = train_test_split(x_test_temp, y_test_temp, 
+                                                            test_size=0.33, random_state=0) 
+
+
+# In[39]:
+
+
+print('Test Train Dev split done. Length of x_train, y_train,x_dev, y_dev, x_test, y_test are', 
+     list(map(len, [x_train, y_train,x_dev, y_dev, x_test, y_test])))
+
+
+# In[40]:
+
+
+from tensorflow.keras.layers import Input, LSTM, Dense, Concatenate, TimeDistributed, Bidirectional, Embedding
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+from attention_keras.layers.attention import AttentionLayer
+from tensorflow.keras.callbacks import TensorBoard
+
+from tensorflow.keras import backend as K 
+K.clear_session() 
+hidden_units = 200 #this is 600 in the paper. To be changed later.
+
+
+# In[41]:
+
+
+enc_embedding_layer = Embedding(len(x_word_index) + 1,
+                            EMBEDDING_DIM,
+                            weights=[encoder_emb],
+                            input_length=MAX_TEXT_LENGTH,
+                            trainable=False,
+                            name='EncoderEmbeddingLayer')
+
+
+# In[42]:
+
+
+dec_embedding_layer = Embedding(len(y_word_index) + 1,
+                            EMBEDDING_DIM,
+                            weights=[decoder_emb],
+                            input_length=MAX_HEADLINE_LENGTH,
+                            trainable=False,
+                            name='DecoderEmbeddingLayer')
+
+
+# In[43]:
+
+
+# Encoder 
+
+# Encoder input 
+# 2D (sequence_length, None), where sequence length is the MAX_LEN unified by padding in preprocessing
+encoder_inputs = Input(shape=(MAX_TEXT_LENGTH,), name="EncoderInput") 
+enc_emb = enc_embedding_layer(encoder_inputs) 
+
+
+#LSTM 1 
+encoder_lstm1 = LSTM(hidden_units,return_sequences=True,return_state=True, name='EncLSTM1') 
+encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb) 
+
+#LSTM 2 
+encoder_lstm2 = LSTM(hidden_units,return_sequences=True,return_state=True, name='EncLSTM2') 
+encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1) 
+
+#LSTM 3 
+encoder_lstm3=LSTM(hidden_units, return_state=True, return_sequences=True, name='EncLSTM3') 
+encoder_output3, state_h3, state_c3= encoder_lstm3(encoder_output2) 
+
+#LSTM 4 
+encoder_lstm4=LSTM(hidden_units, return_state=True, return_sequences=True, name='EncLSTM4') 
+encoder_outputs, state_h, state_c= encoder_lstm4(encoder_output3) 
+
+
+# In[44]:
+
+
+# Decoder 
+
+decoder_inputs = Input(shape=(None,), name = 'DecoderInput') 
+#dec_emb_layer = Embedding(y_voc_size, latent_dim,trainable=True) 
+dec_emb = dec_embedding_layer(decoder_inputs) 
+
+#LSTM using encoder_states as initial state
+decoder_lstm = LSTM(hidden_units, return_sequences=True, return_state=True, name='DecLSTM1') 
+decoder_outputs,decoder_fwd_state, decoder_back_state = decoder_lstm(dec_emb,initial_state=[state_h, state_c]) 
+
+#Attention Layer
+attn_layer = AttentionLayer(name='attention_layer') 
+attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs]) 
+
+# Concat attention output and decoder LSTM output 
+decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([decoder_outputs, attn_out])
+
+#Dense layer
+decoder_dense = TimeDistributed(Dense(len(y_word_index)+1, activation='softmax')) 
+decoder_outputs = decoder_dense(decoder_concat_input) 
+
+
+# In[45]:
+
+
+# Define the model
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs) 
+model.summary()
+
+
+# In[46]:
+
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+#model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy')
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+
+# In[47]:
+
+
+history=model.fit([x_train,np.hstack((np.zeros((y_train.shape[0],1)), y_train[:, :-1]))], 
+                  y_train,
+                  epochs=4,
+                  batch_size=200, 
+                  callbacks=[tensorboard],
+                  validation_data=([x_dev,np.hstack((np.zeros((y_dev.shape[0],1)), y_dev[:, :-1]))], y_dev)
+                 )
+
+
+# In[48]:
+
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model_fd_111319_250units_punct.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model_fd_111319_250units_punct.h5")
+print("Saved model and weights to the disk")
+
+
+# In[51]:
+
+
+pickle.dump(history.history, open( historyfilename, "wb" ) )
+print("Saved model history object to the disk.")
+
---
+++ preparedata.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+#####
+# Packages
+##### 
+
+import numpy as np
+import pandas as pd 
+import re #for regex search purposes          
+import pickle
+cleanfilename = 'cleandata.pkl'
+wikihowfile = 'wikihowSep.csv'
+
+
+# In[19]:
+
+
+sepdata = pd.read_csv(wikihowfile)
+
+
+# In[20]:
+
+
+print ('Read file. Shape is ', sepdata.shape)
+
+
+# In[21]:
+
+
+sepdata_v1 = sepdata.dropna(subset=['headline','text'], axis=0).reset_index(drop=True) 
+
+
+# In[22]:
+
+
+print ('Removed NAs. Shape is ', sepdata_v1.shape)
+
+
+# In[23]:
+
+
+# List of contractions that we will map to 
+
+contraction_mapping = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not",
+
+                           "didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not",
+
+                           "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",
+
+                           "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would",
+
+                           "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would",
+
+                           "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam",
+
+                           "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have",
+
+                           "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock",
+
+                           "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have",
+
+                           "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is",
+
+                           "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as",
+
+                           "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would",
+
+                           "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have",
+
+                           "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have",
+
+                           "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are",
+
+                           "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",
+
+                           "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is",
+
+                           "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have",
+
+                           "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have",
+
+                           "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all",
+
+                           "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have",
+
+                           "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have",
+
+                           "you're": "you are", "you've": "you have"}
+
+
+# In[24]:
+
+
+def text_cleaner(text):
+    # Step 0: Convert to string in case a float or int is found.
+    newString = str(text)
+    # Step 1: Lower case the text 
+    newString = newString.lower()
+    # Step 2: Get rid of commas
+    #newString = re.sub(r'\([^)]*\)', '', newString)
+    # Step 3: Get rid of quotations 
+    newString = re.sub('"','', newString)
+    # Step 4: get rid of contractions with our contraction mapping 
+    newString = ' '.join([contraction_mapping[t] if t in contraction_mapping else t for t in newString.split(" ")])  
+    # Step 5: get rid of the \n stuff 
+    newString = re.sub(r"'s\n","",newString)
+    # Step 6: anything that is a number, get rid of it 
+    newString = re.sub("\d", " ", newString) 
+    # Step 7: separating punctuations so they are their own tokens
+    newString = re.sub(r'([,.!;:?])', r' \1', newString)
+
+    return newString.split()
+
+
+# In[25]:
+
+
+print("Starting to clean data")
+
+
+# In[26]:
+
+
+clean_data = pd.DataFrame()
+
+clean_data['text'] = sepdata_v1['text'].apply(text_cleaner)
+
+
+# In[27]:
+
+
+print("Text clean complete. Number of items is ", len(clean_data.text))
+
+
+# In[28]:
+
+
+clean_data['headline'] = sepdata_v1['headline'].apply(text_cleaner)
+
+
+# In[29]:
+
+
+print("Title cleaning complete. Number of items is ", len(clean_data.headline))
+
+
+# In[30]:
+
+
+print('Saving file to disk. File name is ', cleanfilename)
+
+
+# In[31]:
+
+
+pickle.dump( clean_data, open( cleanfilename, "wb" ) )
+
+
+# In[33]:
+
+
+print('File saved.')
+
+
+# In[35]:
+
+
+tp = pickle.load( open( cleanfilename, "rb" ) )
+
+
+# In[37]:
+
+
+tp
+
